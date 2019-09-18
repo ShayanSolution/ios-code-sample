8f8d9539df2d02bdc0c6244262a9535e938732d6
dev4 <dev1@shayansolutions.com>
2019-09-18 11:26:17 +0500

    Initial commit for ios-code-sample

---
+++ FindRoute/FindRoute/ViewController.swift
@@ -0,0 +1,19 @@
+/**
+ * Class ViewController.swift
+ * @package     Application
+ * @author      Arslan Ali
+ * @email       shayansolutions@gmail.com
+ */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
